81da9a650a0a21ce54bd7e5f9e0a6a46fed6a25a
jakarai <jakarai@utu.fi>
2018-01-09 13:17:33 +0200

    manager overload

--- DownloadManager.java
+++ DownloadManager.java
@@ -6,6 +6,21 @@ import java.util.Scanner;
 
 public class DownloadManager {
 
+    public static void download() throws Exception{
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Input file URL : ");
+        String url = reader.nextLine();
+        URI u = URI.create(url);
+        System.out.println("Input file name.");
+        Path path = Paths.get(reader.nextLine());
+        try (InputStream in = u.toURL().openStream()) { //stream from URL
+            Files.copy(in, path);       //copy from stream to file path
+            System.out.println("\n--***--\nDownload complete.\n--***--\n");
+        } catch (FileAlreadyExistsException faeE) {
+            System.out.println("A file with that name already exists. Enter a new name : ");
+            download(url, reader.nextLine());
+        }
+    }
     public static void download(String url, String file) throws Exception {
         Path path = Paths.get(file); //file path
         URI u = URI.create(url);
@@ -18,5 +33,6 @@ public class DownloadManager {
             download(url, reader.nextLine());
         }
     }
+
 
 }
