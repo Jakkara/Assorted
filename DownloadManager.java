ccecdb87d113583a2d076b72e9fa07ae9990fd05
jakarai <jakarai@utu.fi>
2018-01-09 13:12:27 +0200

    download works

--- DownloadManager.java
+++ DownloadManager.java
@@ -1,22 +1,21 @@
-import java.io.File;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
-import java.nio.*;
 import java.nio.file.*;
 import java.util.Scanner;
+
 
 public class DownloadManager {
 
     public static void download(String url, String file) throws Exception {
-        Path path = Paths.get(file);
+        Path path = Paths.get(file); //file path
         URI u = URI.create(url);
-        try (InputStream in = u.toURL().openStream()) {
-            Files.copy(in, path);
-            System.out.println("Download complete.");
-        }catch (FileAlreadyExistsException faeE){
-            faeE.printStackTrace();
+        try (InputStream in = u.toURL().openStream()) { //stream from URL
+            Files.copy(in, path);       //copy from stream to file path
+            System.out.println("\n--***--\nDownload complete.\n--***--\n");
+        } catch (FileAlreadyExistsException faeE) {
+            System.out.println("A file with that name already exists. Enter a new name : ");
+            Scanner reader = new Scanner(System.in);
+            download(url, reader.nextLine());
         }
     }
 
--- XMLTest.java
+++ XMLTest.java
@@ -1,32 +1,36 @@
 import org.xml.sax.*;
 import org.w3c.dom.*;
+
 import javax.xml.parsers.*;
 import java.util.Scanner;
 
 public class XMLTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         DownloadManager downloader = new DownloadManager();
         System.out.println("Input download URL : ");
         String url = reader.nextLine();
         System.out.println("Input file name : ");
-        String fileName =  reader.nextLine();
-        try{
-            downloader.download(url,fileName);
-        }catch(Exception e){e.printStackTrace();}
+        String fileName = reader.nextLine();
+        try {
+            downloader.download(url, fileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         Document xmlDoc = getDocument(fileName);
         System.out.println("File found : " + xmlDoc.getDocumentElement().getNodeName());
         Node rootLayer = xmlDoc.getElementsByTagName("Layer").item(0);
         NodeList layers = findNodes(rootLayer, "Layer");
         System.out.println("Available layers: " + layers.getLength());
         for (int i = 0; i < layers.getLength(); i++) {
-            Node title = findNodes(layers.item(i),"Title").item(0);
+            Node title = findNodes(layers.item(i), "Title").item(0);
             System.out.println(title.getTextContent());
         }
     }
 
     private static Document getDocument(String docString) {
-        try{
+        try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             factory.setIgnoringComments(true);
             factory.setIgnoringElementContentWhitespace(true);
@@ -34,26 +38,30 @@ public class XMLTest {
 
             DocumentBuilder builder = factory.newDocumentBuilder();
             return builder.parse(new InputSource(docString));
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return null;
     }
-    public static void printTags(Node nodes){
-        if(nodes.hasChildNodes()  || nodes.getNodeType()!=3){
-            System.out.println(nodes.getNodeName()+" : "+nodes.getTextContent());
-            NodeList nl=nodes.getChildNodes();
-            for(int j=0;j<nl.getLength();j++)printTags(nl.item(j));
+
+    public static void printTags(Node nodes) {
+        if (nodes.hasChildNodes() || nodes.getNodeType() != 3) {
+            System.out.println(nodes.getNodeName() + " : " + nodes.getTextContent());
+            NodeList nl = nodes.getChildNodes();
+            for (int j = 0; j < nl.getLength(); j++) printTags(nl.item(j));
         }
     }
-    public static Element toElement(Node nd){
-        if (nd instanceof Element){
+
+    public static Element nodeToElement(Node nd) {
+        if (nd instanceof Element) {
             Element docElement = (Element) nd;
             return docElement;
-        }return null;
+        }
+        return null;
     }
-    public static NodeList findNodes(Node nd, String name){
-        Element ele = toElement(nd);
+
+    public static NodeList findNodes(Node nd, String name) {
+        Element ele = nodeToElement(nd);
         return ele.getElementsByTagName(name);
     }
 }
